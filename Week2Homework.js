df8584a968fbff7ad148e1573fc96e31917c6c9d
delorean williams <deloreanwilliams25@gmail.com>
2024-02-04 21:17:50 -0600

    week2homework

---
+++ Week2Homework.js
@@ -0,0 +1,14 @@
+let pizzaPlace;
+let numberOfToppings;
+
+let pizzaPalace = pizzaPlace;
+let five = numberOfToppings;
+
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+
+let chicago = `we have the best pizza on the planet here at ${pizzaPlace} where we have the best ingredients with ${numberOfToppings} number of different toppings `;
+if (numberOfToppings < 10);
+{
+  console.log("Quality, not quantity");
+}
