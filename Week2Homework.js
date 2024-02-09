5868a261afdd09199dae30c17362f0dde7b06a72
delorean williams <deloreanwilliams25@gmail.com>
2024-02-09 14:24:38 -0600

    updated week2homework

--- Week2Homework.js
+++ Week2Homework.js
@@ -1,13 +1,11 @@
-let pizzaPlace;
-let numberOfToppings;
+let pizzaPlace = "PizzaPalace";
+let numberOfToppings = 5;
 
-let pizzaPalace = pizzaPlace;
-let five = numberOfToppings;
-
-console.log(typeof pizzaPlace);
-console.log(typeof numberOfToppings);
+console.log(pizzaPlace, typeof pizzaPlace);
+console.log(numberOfToppings, typeof numberOfToppings);
 
 let chicago = `we have the best pizza on the planet here at ${pizzaPlace} where we have the best ingredients with ${numberOfToppings} number of different toppings `;
+console.log(chicago);
 if (numberOfToppings < 10);
 {
   console.log("Quality, not quantity");
